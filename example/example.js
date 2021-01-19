e1c48cbc392a84f395b52b90de0df6df0a9d8b94
kumavis <aaron@kumavis.me>
2021-01-19 23:10:20 +0800

    init commit

---
+++ example/example.js
@@ -0,0 +1,245 @@
+import {
+  // util
+  count, delay, delayWithContext, background,
+  // async
+  asyncForEach, asyncFlatten, asyncMap,
+  // parallel
+  parallelForEach, parallelMap, parallelReduce,
+  // stream
+  stream, streamWithContext, pipe, pump,
+  // kumavis
+  asyncIterToProducer, connect, pipeline
+} from '../src/index.js'
+
+const demoAsyncForEach = async () => {
+  console.log('demo serial async for each');
+  await asyncForEach(count(10), async (n) => {
+    await delay(Math.random() * 100);
+    console.log(n);
+  });
+};
+
+const demoParallelForEach = async () => {
+  console.log('demo parallel async for each');
+  await parallelForEach(5, count(20), async (n) => {
+    await delay(Math.random() * 100);
+    console.log(n);
+  });
+};
+
+const demoParallelReduce = async () => {
+  console.log('demo parallel reduce');
+
+  const sum = await parallelReduce(10, 0, count(10), async (a, b) => {
+    if (a == 0) {
+      return b;
+    }
+    await delay(Math.random() * 100);
+    console.log(a, '+', b, '=', a + b);
+    return a + b;
+  });
+
+  console.log(sum);
+};
+
+const demoPipe = async () => {
+  console.log('demo pipe');
+
+  // eager producer
+  const producer = async (output) => {
+    for (const token of count(10)) {
+      console.log(token, '->');
+      await output.next(token);
+    }
+    output.return();
+  };
+
+  const consumer = async (input) => {
+    for await (const token of input) {
+      console.log('->', token);
+      await delay(Math.random() * 100);
+    }
+  };
+
+  const [input, output] = pipe();
+
+  await Promise.all([
+    producer(output),
+    consumer(input),
+  ]);
+};
+
+const demoPump = async () => {
+  console.log('demo pump');
+
+  // lazy producer
+  async function *producer() {
+    for (const token of count(10)) {
+      console.log(token, '->');
+      // ??? await yield?
+      await(yield token);
+    }
+  };
+
+  const consumer = async (input) => {
+    for await (const token of input) {
+      console.log('->', token);
+      await delay(Math.random() * 100);
+    }
+  };
+
+  const [input, output] = pipe();
+
+  await Promise.all([
+    pump(output, producer()),
+    consumer(input),
+  ]);
+};
+
+const demoAsyncFlatten = async () => {
+  console.log('demo async flatten');
+  for await (const value of asyncFlatten([count(3), count(3)])) {
+    console.log(value);
+  }
+};
+
+const demoAsyncFlattenMap = async () => {
+  console.log('demo async flatten');
+  const streams = asyncMap(count(3), () => count(3));
+  for await (const value of asyncFlatten(streams)) {
+    console.log(value);
+  }
+};
+
+const demoParallelMap = async () => {
+  console.log('demo parallel map');
+  const streams = parallelMap(7, count(3), async n => {
+    await delay(Math.random() * 100);
+    return asyncMap(count(5), async m => {
+      await delay(Math.random() * 100);
+      return (m+1) * 10 + n;
+    })
+  });
+  for await (const value of asyncFlatten(streams)) {
+    console.log(value);
+  }
+};
+
+const demoStreamTimeout = async () => {
+  console.log('streaming with a timeout');
+  const context = background.withTimeout(1000);
+  const stream = streamWithContext(context, count(1000));
+  try {
+    await parallelForEach(10, stream, async (n) => {
+      await delayWithContext(context, Math.random() * 1000);
+      console.log(n);
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+// kumavis experiments
+
+const demoConnect = async () => {
+  console.log('demo connect');
+
+  // lazy producer
+  async function *producer() {
+    for (const token of count(10)) {
+      console.log(token, '->');
+      // ??? await yield?
+      await(yield token);
+    }
+  };
+
+  const consumer = async (input) => {
+    for await (const token of input) {
+      console.log('->', token);
+      await delay(Math.random() * 100);
+    }
+  };
+
+  await connect(
+    asyncIterToProducer(producer),
+    consumer
+  );
+
+};
+
+
+const demoDuplex = async () => {
+  console.log('demo duplex');
+
+  const network = {
+    async producer (output) {
+      for (const token of count(10)) {
+        console.log('network.inbound <--', token);
+        await output.next(token);
+      }
+      output.return();
+    },
+    async consumer (input) {
+      for await (const token of input) {
+        console.log('network.outbound -->', token);
+        await delay(Math.random() * 100);
+      }
+    },
+  };
+
+  // hold it
+  // const encoding = {
+  //   async producer (output) {
+  //     for (const token of count(10)) {
+  //       console.log('node.inbound <--', token);
+  //       await output.next(token);
+  //     }
+  //     output.return();
+  //   },
+  //   async consumer (input) {
+  //     for await (const token of input) {
+  //       console.log('node.outbound -->', token);
+  //       await delay(Math.random() * 100);
+  //     }
+  //   },
+  // };
+
+  const node = {
+    async producer (output) {
+      for (const token of count(10)) {
+        console.log('node.inbound <--', token);
+        await output.next(token);
+      }
+      output.return();
+    },
+    async consumer (input) {
+      for await (const token of input) {
+        console.log('node.outbound -->', token);
+        await delay(Math.random() * 100);
+      }
+    },
+  };
+  
+  await pipeline(
+    network,
+    node,
+    network,
+  )
+
+};
+
+
+
+(async () => {
+  await demoAsyncForEach();
+  await demoParallelForEach();
+  await demoParallelReduce();
+  await demoAsyncFlatten();
+  await demoAsyncFlattenMap();
+  await demoParallelMap();
+  await demoPipe();
+  await demoPump();
+  await demoStreamTimeout();
+  await demoConnect();
+  await demoDuplex();
+})();
---
+++ src/index.js
@@ -0,0 +1,231 @@
+
+export function *count(n) {
+  for (let i = 0; i < n; i++) {
+    yield i;
+  }
+}
+
+export const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+export const asyncForEach = async (values, callback) => {
+  for await (const value of values) {
+    await callback(value);
+  }
+};
+
+export const parallel = (limit, callback) => {
+  function *workers() {
+    for (const worker of count(limit)) {
+      yield callback(worker);
+    }
+  }
+  return Promise.all(workers());
+};
+
+export const parallelForEach = async (limit, values, callback) => {
+  return parallel(limit, () => asyncForEach(values, callback));
+};
+
+export const asyncReduce = async (zero, values, callback) => {
+  for await (const value of values) {
+    zero = await callback(zero, value);
+  }
+  return zero;
+};
+
+export const parallelReduce = async (limit, zero, values, callback) => {
+  values = await parallel(limit, () => asyncReduce(zero, values, callback));
+  return asyncReduce(zero, values, callback);
+};
+
+export const defer = () => {
+  let resolve, reject;
+  const promise = new Promise((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+  return { promise, resolve, reject };
+};
+
+export const queue = () => {
+  const ends = defer();
+  return {
+    put(value) {
+      const next = defer();
+      const promise = next.promise;
+      ends.resolve({ value, promise });
+      ends.resolve = next.resolve;
+    },
+    get() {
+      const promise = ends.promise.then(next => next.value);
+      ends.promise = ends.promise.then(next => next.promise);
+      return promise;
+    },
+  };
+};
+
+// stream creates a stream, which waits for the consumer
+// which threads two queues,
+// up = for writing values to
+// down = for awaiting readiness
+export const stream = (up, down) => ({
+  next(value) {
+    up.put({ value, done: false });
+    return down.get();
+  },
+  return(value) {
+    up.put({ value, done: true });
+    return down.get();
+  },
+  throw(error) {
+    up.put(Promise.reject(error));
+    return down.get();
+  },
+  [Symbol.asyncIterator]() {
+    return this;
+  },
+});
+
+// pipe creates a pipe, for connecting consumer and producer
+// output = inbox, for the producer to populate
+// input = outbox, for the consumer to read from
+export const pipe = () => {
+  const syn = queue();
+  const ack = queue();
+  const input = stream(syn, ack);
+  const output = stream(ack, syn);
+  return [input, output];
+};
+
+// pump pulls from one stream and pushes to another.
+// The pump slows down for output back-pressure.
+// (connects async iter to queue)
+// ?? language? stream / async iter?
+// output = stream where values are written to
+// input = async iter where values are pulled from
+export const pump = async (output, input) => {
+  try {
+    let value, done;
+    while ({value, done} = await input.next()) {
+      if (done) {
+        return output.return(value);
+      }
+      await output.next(value);
+    }
+  } catch (error) {
+    return output.throw(error);
+  }
+};
+
+export async function *asyncFlatten(streams) {
+  for await (const stream of streams) {
+    for await (const value of stream) {
+      yield value;
+    }
+  }
+}
+
+export async function *asyncMap(values, callback) {
+  for await (const value of values) {
+    // ??? await yield await, really?
+    await(yield await callback(value));
+  }
+}
+
+export const parallelMap = (limit, values, callback) => {
+  const [input, output] = pipe();
+  parallel(limit, () => pump(input, asyncMap(values, callback)));
+  return output;
+}
+
+export const delayWithContext = (context, ms) => {
+  const { promise, resolve, reject } = defer();
+  let handle = setTimeout(resolve, ms);
+  context.cancelled.catch((error) => {
+    reject(error);
+    clearTimeout(handle);
+  });
+  return promise;
+};
+
+export const never = defer().promise;
+
+export const background = Object.freeze({
+  cancelled: never,
+  deadline: Infinity,
+  with(child) {
+    return Object.freeze({
+      __proto__: this,
+      ...child,
+    });
+  },
+  withTimeout(timeout) {
+    const deadline = Date.now() + timeout;
+    return this.withTimeoutAndDeadline(timeout, deadline);
+  },
+  withDeadline(deadline) {
+    const timeout = deadline - Date.now();
+    return this.withTimeoutAndDeadline(timeout, deadline);
+  },
+  withTimeoutAndDeadline(timeout, deadline) {
+    if (deadline > this.deadline) {
+      return this;
+    }
+    const { cancel, context } =  this.withCancel();
+    delayWithContext(this, timeout).then(() => cancel(new Error(`Timed out`)));
+    return context.with({ deadline });
+  },
+  withCancel() {
+    const { promise, reject } = defer();
+    const context = this.with({ cancelled: promise });
+    this.cancelled.catch(reject);
+    return {cancel: reject, context};
+  },
+});
+
+export const streamWithContext = (context, stream) => ({
+  next(value) {
+    return Promise.race([context.cancelled, stream.next(value)]);
+  },
+  return(value) {
+    return Promise.race([context.cancelled, stream.return(value)]);
+  },
+  throw(error) {
+    return Promise.race([context.cancelled, stream.throw(error)]);
+  },
+  [Symbol.asyncIterator]() {
+    return this;
+  },
+});
+
+// kumavis experiments
+
+export function asyncIterToProducer(asyncGen) {
+  return async function producer (output) {
+    const input = asyncGen();
+    return pump(output, input);
+  }
+}
+
+export async function connect (producer, consumer) {
+  const [input, output] = pipe();
+  await Promise.all([
+    producer(output),
+    consumer(input),
+  ]);
+}
+
+export async function pipeline (...duplexes) {
+  console.log(duplexes)
+  const connections = []
+  for (const _index in duplexes) {
+    const index = parseInt(_index)
+    if (index === 0) continue
+    const source = duplexes[index - 1]
+    const dest = duplexes[index]
+    connections.push(
+      connect(source.producer, dest.consumer)
+    )
+  }
+  await Promise.all(connections)
+}
